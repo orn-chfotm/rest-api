83ccfc0fc036690453344e2de7db2f5454cd842a
dh_kim <dh_kim@easymedia.net>
2024-03-25 16:51:10 +0900

    2024-03-25 Security 허용 경로 설정

---
+++ src/main/java/com/spring/restapi/core/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.spring.restapi.core.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Slf4j
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers(new AntPathRequestMatcher("/h2-console/**"))
+                .requestMatchers(new AntPathRequestMatcher("/favicon.ico"))
+                .requestMatchers(new AntPathRequestMatcher("/lib/**"));
+    }
+
+
+}
